f8cd7bec2c85b67d6f9803bfd7da551fb69845c6
mfrattini7 <marcello.frattini7@gmail.com>
2025-12-08 13:09:30 +0100

    error handling added

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,7 @@
+require("dotenv").config()
 const express = require('express')
-var morgan = require('morgan')
-
+const morgan = require('morgan')
+const Person = require("./models/person")
 const app = express()
 
 app.use(express.json())
@@ -11,62 +12,31 @@ morgan.token('body', req => {
 })
 app.use(morgan(':method :url :body'))
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(
+    persons => response.json(persons)
+  )
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = persons.find(person => person.id === id)
-    if (person) {
-      response.json(person)
-    } else {
-      response.status(404).end()  
-    }
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }      
+  }).catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  persons = persons.filter(note => note.id !== id)
-
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
-
-/* They want us to use a worse method with Math.random (see below)
-const generateId = () => {
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(n => Number(n.id)))
-    : 0
-  return String(maxId + 1)
-}
-*/
-
-const generateId = () => {
-    return "" + Math.floor(Math.random() * 1000000000);
-}
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
@@ -83,19 +53,14 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
-  if (persons.map(it => it.name).includes(body.name)) {
-    return response.status(409).json({
-      error: `name must be unique`
-    })
-  }
-
-  const note = {
-    id: generateId(),
+  const person = new Person({
     name: body.name,
     number: body.number,
-  }
-  persons = persons.concat(note)
-  response.json(note)
+  })
+
+  person.save().then(savedPerson => {
+    response.json(person)
+  })
 })
 
 app.get('/info', (request, response) => {
@@ -104,8 +69,29 @@ app.get('/info', (request, response) => {
   return response.send(`Phonebook has info for ${num} people.<br>${date}`)
 })
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
-const PORT = 3001
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
+
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
