b62990f4f7cc93cda9e738de381efa595d4dec8d
mfrattini7 <marcello.frattini7@gmail.com>
2025-12-08 19:04:46 +0100

    finished 4.2

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,38 +1,7 @@
+const app = require('./app') // the actual Express application
 const config = require('./utils/config')
-const express = require('express')
-const mongoose = require('mongoose')
+const logger = require('./utils/logger')
 
-const app = express()
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl, { family: 4 })
-
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
-
-const PORT = config.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
